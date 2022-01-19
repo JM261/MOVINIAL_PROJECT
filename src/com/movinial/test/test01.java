a9ab952431502f859bbb8975ccff4a3f8ac6978c
고정인 <@DESKTOP-5EC95EC>
2022-01-19 16:52:05 +0900

    test클래스에서 출력문을 추가하였습니다

--- src/com/movinial/test/test01.java
+++ src/com/movinial/test/test01.java
@@ -3,7 +3,7 @@ package com.movinial.test;
 public class test01 {
 	public static void main(String[] args) {
 		
-		System.out.println("hello world");
+		System.out.println("hello world!!");
 	}
 
 }
