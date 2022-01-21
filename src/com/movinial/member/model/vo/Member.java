c652228ba3d347a8fa4627bd4abcfbaaa9f17182
joohyeon <joohyeon9999@gmail.com>
2022-01-21 14:20:01 +0900

    프로필 사진, 사이드바, member클래스 수정

--- src/com/movinial/member/model/vo/Member.java
+++ src/com/movinial/member/model/vo/Member.java
@@ -15,11 +15,14 @@ public class Member {
 	private String staus;
 	private Date enrollDate;
 	private Date modifyDate;
+	private String preferGenre;
+	
 	public Member() {
 		super();
 	}
+
 	public Member(int userNo, String userId, String userPwd, String userName, String userNickName, String email,
-			String phone, String usertype, String staus, Date enrollDate, Date modifyDate) {
+			String phone, String usertype, String staus, Date enrollDate, Date modifyDate, String preferGenre) {
 		super();
 		this.userNo = userNo;
 		this.userId = userId;
@@ -32,83 +35,122 @@ public class Member {
 		this.staus = staus;
 		this.enrollDate = enrollDate;
 		this.modifyDate = modifyDate;
+		this.preferGenre = preferGenre;
 	}
+	
+	public Member(String userId, String userName, String userNickName, String email, String phone, String preferGenre) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.userNickName = userNickName;
+		this.email = email;
+		this.phone = phone;
+		this.preferGenre = preferGenre;
+	}
+
 	public int getUserNo() {
 		return userNo;
 	}
+
 	public void setUserNo(int userNo) {
 		this.userNo = userNo;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getUserPwd() {
 		return userPwd;
 	}
+
 	public void setUserPwd(String userPwd) {
 		this.userPwd = userPwd;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getUserNickName() {
 		return userNickName;
 	}
+
 	public void setUserNickName(String userNickName) {
 		this.userNickName = userNickName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getUsertype() {
 		return usertype;
 	}
+
 	public void setUsertype(String usertype) {
 		this.usertype = usertype;
 	}
+
 	public String getStaus() {
 		return staus;
 	}
+
 	public void setStaus(String staus) {
 		this.staus = staus;
 	}
+
 	public Date getEnrollDate() {
 		return enrollDate;
 	}
+
 	public void setEnrollDate(Date enrollDate) {
 		this.enrollDate = enrollDate;
 	}
+
 	public Date getModifyDate() {
 		return modifyDate;
 	}
+
 	public void setModifyDate(Date modifyDate) {
 		this.modifyDate = modifyDate;
 	}
-	
+
+	public String getPreferGenre() {
+		return preferGenre;
+	}
+
+	public void setPreferGenre(String preferGenre) {
+		this.preferGenre = preferGenre;
+	}
+
 	@Override
 	public String toString() {
 		return "Member [userNo=" + userNo + ", userId=" + userId + ", userPwd=" + userPwd + ", userName=" + userName
 				+ ", userNickName=" + userNickName + ", email=" + email + ", phone=" + phone + ", usertype=" + usertype
-				+ ", staus=" + staus + ", enrollDate=" + enrollDate + ", modifyDate=" + modifyDate + "]";
+				+ ", staus=" + staus + ", enrollDate=" + enrollDate + ", modifyDate=" + modifyDate + ", preferGenre="
+				+ preferGenre + "]";
 	}
-
-	
-	
 	
 	
 
